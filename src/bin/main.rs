47d4bae842f83070edd86cf454384e2ba5a79711
Dario Goetz <dario.goetz@googlemail.com>
2019-12-24 00:06:07 +0100

    [mod] specify default values with clap

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -30,35 +30,36 @@ fn main() {
         .author("Dario Goetz <dario.goetz@googlemail.com>")
         .about("Serves websockets with messages from surfjudge webserver via ZMQ")
         .arg(Arg::with_name("ws hostname")
-                 .short("w")
-                 .long("websocket-host")
-                 .takes_value(true)
-                 .help("Hostname of websocket server"))
+             .short("w")
+             .long("websocket-host")
+             .default_value("0.0.0.0")
+             .value_name("hostname")
+             .help("Hostname of websocket server"))
         .arg(Arg::with_name("ws port")
-                 .short("p")
-                 .long("websocket-port")
-                 .takes_value(true)
-                 .help("Port of websocket server"))
+             .short("p")
+             .long("websocket-port")
+             .default_value("6544")
+             .value_name("port")
+             .help("Port of websocket server"))
         .arg(Arg::with_name("zmq port")
-                 .short("z")
-                 .long("zeromq-port")
-                 .takes_value(true)
+             .short("z")
+             .long("zeromq-port")
+             .value_name("port")
+             .default_value("6545")
+
              .help("Port of ZeroMQ server"))
     .get_matches();
 
     let _ = env_logger::try_init();
 
     let websocket_host = env::var("WEBSOCKETS_HOST")
-        .unwrap_or(matches.value_of("ws hostname")
-                   .unwrap_or("0.0.0.0").to_string());
+        .unwrap_or(matches.value_of("ws hostname").unwrap().to_string());
     let websocket_port = env::var("WEBSOCKETS_PORT")
-        .unwrap_or(matches.value_of("ws port")
-                   .unwrap_or("6544").to_string());
+        .unwrap_or(matches.value_of("ws port").unwrap().to_string());
     let websocket_addr = format!("{}:{}", websocket_host, websocket_port);
 
     let zmq_port = env::var("ZMQ_PORT")
-        .unwrap_or(matches.value_of("zmq port")
-                   .unwrap_or("6545").to_string());
+        .unwrap_or(matches.value_of("zmq port").unwrap().to_string());
     let zmq_addr = format!("tcp://*:{}", zmq_port);
 
     let mut server = WebSocketServer::new(&websocket_addr, &zmq_addr);
